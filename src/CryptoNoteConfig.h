63f1a14a40bcf76f0b717a12843bd996d3b523a9
Sergei Bragin <45129424+Diz-Kragnet@users.noreply.github.com>
2020-09-04 16:26:10 +0300

    GENESIS_COINBASE_TX_HEX

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "kragnetcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121012f17b6f439f11995911f76576592b0be22a685d5c369cd76e541dcd5c73a21ec";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
